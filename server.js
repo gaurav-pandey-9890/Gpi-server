68601ad1313d01a6e36a097fdadf30f685740b5f
gaurav-pandey-9890 <gauravp989043@gmail.com>
2024-10-05 11:35:29 +0530

    added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js
 
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Import the cors middleware
@@ -10,9 +12,10 @@ const port = process.env.PORT || 3001;
 app.use(cors()); 
 
 // MongoDB connection 
-mongoose.connect('mongodb://localhost:27017/index', { 
+mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: 'index'
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
